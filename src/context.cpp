ee3b821288cb7ce6009bc81432c4fd42cd04398c
saint84ab <saint84ab@gmail.com>
2021-03-27 17:10:39 +0900

    그라데이션 사각형

--- src/context.cpp
+++ src/context.cpp
@@ -9,28 +9,32 @@ ContextUPtr Context::Create() {
 
 bool Context::Init() {
 	
+   	
     float vertices[] = {
-        0.5f, 0.5f, 0.0f, // top right
-        0.5f, -0.5f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        -0.5f, 0.5f, 0.0f, // top left
+         0.5f, 0.5f, 0.0f, 0.0f, 0.7f, 0.3f, // top right, red
+         0.5f, -0.5f, 0.0f, 0.6f, 0.0f, 0.4f, // bottom right, green
+        -0.5f, -0.5f, 0.0f, 0.1f, 0.9f, 0.0f, // bottom left, blue
+        -0.5f, 0.5f, 0.0f, 0.3f, 0.3f, 0.4f, // top left, yellow
     };
+
     uint32_t indices[] = { // note that we start from 0!
         0, 1, 3, // first triangle
         1, 2, 3, // second triangle
     };
+
     m_vertexLayout =  VertexLayout::Create();
 
     m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER,
-        GL_STATIC_DRAW, vertices, sizeof(float) * 12);
+        GL_STATIC_DRAW, vertices, sizeof(float) * 24);
 
-    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+    m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, sizeof(float) * 3);
 
     m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER,
         GL_STATIC_DRAW, indices, sizeof(uint32_t) * 6);
 
-    ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
-    ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
+    ShaderPtr vertShader = Shader::CreateFromFile("./shader/per_vertex_color.vs", GL_VERTEX_SHADER);
+    ShaderPtr fragShader = Shader::CreateFromFile("./shader/per_vertex_color.fs", GL_FRAGMENT_SHADER);
     if (!vertShader || !fragShader)
         return false;
     SPDLOG_INFO("vertex shader id: {}", vertShader->Get());
